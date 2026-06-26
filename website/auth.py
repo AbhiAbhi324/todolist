e08dcf4266477831cdb1dbb113ac9d8ca8168779
AbhiAbhi324 <abhiabhi4a@gmail.com>
2026-06-26 19:11:23 +0530

    created the login page ,sign-in page and home page

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,31 @@
-from flask import Blueprint
+from flask import Blueprint,render_template,request,flash
 
 auth =Blueprint("auth",__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return "<h1>LOGIN</h1>"
+    data=request.form
+    print(data)
+    return render_template("login.html",boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<h1>LOGOUT<h1>"
 
-@auth.route('/signin')
+@auth.route('/signin',methods=['GET','POST'])
 def signin():
-    return "<h1>SIGN IN<h1>"
+    data=request.form
+    print(data)
+    if request.method=='POST':
+        Name=request.form.get('name')
+        Email=request.form.get('username')
+        password=request.form.get('password')
+        password2=request.form.get('confirm_password')
+        
+        if password!=password2:
+            flash('password doesnt matches',category='error')
+        else:
+            flash('sucessfully created',category='success')
+            
+        
+    return render_template("signin.html")
